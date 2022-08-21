747dd72b65412c58da006877decb44b3dd77ec94
traxcer <meloy30@hotmail.com>
2022-08-21 21:50:20 +0200

    Actualizo

--- Python/scrapingEnv/scrapetest2.py
+++ Python/scrapingEnv/scrapetest2.py
@@ -1,9 +1,12 @@
 # Primera prueba de scraping en python
 
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup4
 
 html=urlopen('http://pythonscraping.com/pages/page1.html')
 print ('El tipo de html es:%s',type(html))
 bs = BeautifulSoup(html,'html.parser')
 print(bs.h1)
+
+
+
