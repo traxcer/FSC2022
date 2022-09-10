2c2d606563af68c9d8d5e00c9523cbe96ff7610a
traxcer <meloy30@hotmail.com>
2022-09-10 14:07:18 +0200

    Estudio automatizacion

---
+++ Python/AprendePython/Tema_03/ej_01.py
@@ -0,0 +1,17 @@
+# Asigna un valor entero 2001 a la variable space_odyssey y muestra su valor
+
+from unittest import result
+
+
+space_odyssey=2001
+print (space_odyssey)
+
+# Descubre el tipo del literal
+literal="Good night & good luck"
+print (f"El literal: {literal}, es del tipo {type(literal)}")
+
+literal=True
+print (f"El literal: {literal}, es del tipo {type(literal)}")
+
+result=10*3.0
+print (f"El resultado es: {result}, es del tipo {type(result)}")
